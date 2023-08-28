0b6d9110ea33777c0bf51eaaf027b6b2bd8bae12
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-08-28 12:05:39 +0200

    Test that the stream is initialized.

--- test/kmmTest.cu
+++ test/kmmTest.cu
@@ -6,3 +6,11 @@ TEST(MemoryManager, StreamInitialized) {
     auto manager = kmm::MemoryManager();
     EXPECT_EQ(manager.getStream(), nullptr);
 }
+
+TEST(MemoryManager, StreamAllocated) {
+    auto manager = kmm::MemoryManager();
+    unsigned int allocation_id = 0;
+
+    allocation_id = manager.allocate(32);
+    EXPECT_NE(manager.getStream(), nullptr);
+}
