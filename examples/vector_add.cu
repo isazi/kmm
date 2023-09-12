52022dbf61a1b802f814d298f88f42e783542d53
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-09-12 14:34:00 +0200

    Updated example.

--- examples/vector_add.cu
+++ examples/vector_add.cu
@@ -20,31 +20,32 @@ void initialize(void* A, void* B, unsigned int size) {
 }
 
 int main(void) {
+    unsigned int device_id = 0;
     unsigned int threads_per_block = 1024;
     unsigned int n_blocks = ceil((1.0 * SIZE) / threads_per_block);
-    unsigned int A_h, B_h, C_h;
-    unsigned int A_d, B_d, C_d;
+    kmm::Pointer A_h, B_h, C_h;
+    kmm::Pointer A_d, B_d, C_d;
     std::size_t n = SIZE * sizeof(float);
     auto manager = kmm::Manager();
 
     A_h = manager.create(kmm::DeviceType::CPU, n);
     B_h = manager.create(kmm::DeviceType::CPU, n);
     C_h = manager.create(kmm::DeviceType::CPU, n);
-    manager.run(&initialize, kmm::DeviceType::CPU, 0, A_h, B_h, SIZE);
-    A_d = manager.create(kmm::DeviceType::CUDA, 0, n);
-    B_d = manager.create(kmm::DeviceType::CUDA, 0, n);
-    C_d = manager.create(kmm::DeviceType::CUDA, 0, n);
-    manager.copy_to(kmm::DeviceType::CUDA, A_d, n, A_h, 0);
-    manager.copy_to(kmm::DeviceType::CUDA, B_d, n, B_h, 0);
+    manager.run(&initialize, kmm::DeviceType::CPU, device_id, A_h, B_h, SIZE);
+    A_d = manager.create(kmm::DeviceType::CUDA, device_id, n);
+    B_d = manager.create(kmm::DeviceType::CUDA, device_id, n);
+    C_d = manager.create(kmm::DeviceType::CUDA, device_id, n);
+    manager.copy_to(kmm::DeviceType::CUDA, A_d, n, A_h, device_id);
+    manager.copy_to(kmm::DeviceType::CUDA, B_d, n, B_h, device_id);
     // TODO: reimplement execution
-    vector_add<<<threads_per_block, n_blocks, 0, manager.getStream()>>>(
-        reinterpret_cast<float*>(manager.getPointer(A_d)),
-        reinterpret_cast<float*>(manager.getPointer(B_d)),
-        reinterpret_cast<float*>(manager.getPointer(C_d)),
-        SIZE);
-    manager.release(A_d);
-    manager.release(B_d);
-    manager.release(C_d, n, reinterpret_cast<void*>(C_h));
+    // vector_add<<<threads_per_block, n_blocks, 0, manager.getStream()>>>(
+    //     reinterpret_cast<float*>(manager.getPointer(A_d)),
+    //     reinterpret_cast<float*>(manager.getPointer(B_d)),
+    //     reinterpret_cast<float*>(manager.getPointer(C_d)),
+    //     SIZE);
+    manager.release(A_d, device_id);
+    manager.release(B_d, device_id);
+    manager.release(kmm::DeviceType::CUDA, C_d, n, C_h, device_id);
     for (unsigned int item = 0; item < SIZE; item++) {
         if ((C_h[item] - 3.0) > 1.0e-9) {
             std::cout << "ERROR" << std::endl;
