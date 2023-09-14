e3452ad39cec66ec5883075f05fb56fd548411c5
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-09-14 13:25:20 +0200

    Adding more tests, not sure about one of them.

--- test/kmmTest.cu
+++ test/kmmTest.cu
@@ -1,8 +1,29 @@
 #include <gtest/gtest.h>
+#include <type_traits>
 
 #include "kmm.hpp"
 
-TEST(GPU, DeviceZero) {
+TEST(GPU, ZeroInitialization) {
     auto gpu = kmm::GPU();
     EXPECT_EQ(gpu.device_id, 0);
+}
+
+TEST(GPU, Initialization) {
+    auto gpu = KMM::GPU(2);
+    EXPECT_EQ(gpu.device_id, 2);
+}
+
+TEST(GPU, Copy) {
+    auto gpu = KMM::GPU(2);
+    auto new_gpu = gpu;
+    EXPECT_EQ(new_gpu.device_id, gpu.device_id);
+}
+
+TEST(Pointer, Initialized) {
+    auto pointer = kmm::Pointer(14);
+    EXPECT_EQ(pointer.id, 14);
+    auto uint_type = kmm::UInteger();
+    auto new_pointer = kmm::Pointer(3, uint_type);
+    EXPECT_EQ(pointer.id, 3);
+    EXPECT_EQ(std::underlying_type_t<pointer.type>, kmm::UInteger);
 }
