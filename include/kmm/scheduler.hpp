c61bfc6e469929c858b5405aa5b90e9f7d1926d4
stijn <s.heldens@esciencecenter.nl>
2023-10-31 22:43:11 +0100

    Revise Scheduler

--- include/kmm/scheduler.hpp
+++ include/kmm/scheduler.hpp
@@ -1,113 +1,52 @@
 #pragma once
 
+#include <condition_variable>
 #include <deque>
 #include <memory>
+#include <mutex>
 #include <unordered_map>
 #include <utility>
 #include <variant>
 #include <vector>
 
+#include "kmm/command.hpp"
+#include "kmm/executor.hpp"
+#include "kmm/memory.hpp"
 #include "kmm/memory_manager.hpp"
+#include "kmm/object_manager.hpp"
 #include "kmm/types.hpp"
-#include "memory.hpp"
-#include "object_manager.hpp"
 
 namespace kmm {
 
-class ExecutorContext;
-class TaskCompletion;
-class TaskContext;
-
-struct BufferAccess {
-    const Allocation* allocation;
-    bool writable = false;
-};
-
-class Task {
-  public:
-    virtual ~Task() = default;
-    virtual void execute(ExecutorContext&, TaskContext&) = 0;
-};
-
-class Executor {
-  public:
-    virtual ~Executor() = default;
-    virtual void submit(std::shared_ptr<Task>, std::vector<BufferAccess>, TaskCompletion) = 0;
-};
-
-struct BufferRequirement {
-    PhysicalBufferId buffer_id;
-    DeviceId memory_id;
-    bool is_write;
-};
-
-struct CommandExecute {
-    std::optional<ObjectId> output_object_id;
-    DeviceId device_id;
-    std::shared_ptr<Task> task;
-    std::vector<BufferRequirement> buffers;
-};
-
-struct CommandNoop {};
-
-struct CommandBufferCreate {
-    PhysicalBufferId id;
-    BufferLayout description;
-};
-
-struct CommandBufferDelete {
-    PhysicalBufferId id;
-};
-
-struct CommandObjectDelete {
-    ObjectId id;
-};
-
-using Command = std::variant<
-    CommandNoop,
-    CommandExecute,
-    CommandBufferCreate,
-    CommandBufferDelete,
-    CommandObjectDelete>;
-
-struct CommandPacket {
-    CommandPacket(JobId id, Command command, std::vector<JobId> dependencies) :
-        id(id),
-        command(std::move(command)),
-        dependencies(std::move(dependencies)) {}
-
-    JobId id;
-    Command command;
-    std::vector<JobId> dependencies;
-};
-
 class Scheduler: public std::enable_shared_from_this<Scheduler> {
   public:
-    friend class TaskCompletion;
-
-    void make_progress();
+    struct Operation;
+    void make_progress(std::chrono::time_point<std::chrono::system_clock> deadline);
     void submit_command(CommandPacket packet);
 
+    void wakeup_op(const std::shared_ptr<Operation>& op);
+    void complete_op(const std::shared_ptr<Operation>& op);
+
   private:
-    enum class JobStatus { Pending, Ready, Staging, Scheduled, Done };
-    struct Job;
-    struct JobWaker;
+    void trigger_predecessor_completed(const std::shared_ptr<Operation>& op, size_t count = 1);
+    void stage_op(const std::shared_ptr<Operation>& op);
+    void poll_op(const std::shared_ptr<Operation>& op);
+    void schedule_op(const std::shared_ptr<Operation>& op);
 
-    void trigger_predecessor_completed(const std::shared_ptr<Job>&);
-    void schedule_job(const std::shared_ptr<Job>& job);
-    void stage_job(const std::shared_ptr<Job>& job);
-    void complete_job(const std::shared_ptr<Job>&);
+    std::unordered_map<OperationId, std::shared_ptr<Operation>> m_ops;
 
-    std::deque<std::shared_ptr<Job>> m_ready_tasks;
-    std::unordered_map<JobId, std::shared_ptr<Job>> m_jobs;
     std::vector<std::shared_ptr<Executor>> m_executors;
     std::shared_ptr<MemoryManager> m_memory_manager;
     std::shared_ptr<ObjectManager> m_object_manager;
+
+    std::mutex m_ready_lock;
+    std::condition_variable m_ready_cond;
+    std::deque<std::shared_ptr<Operation>> m_ready_ops;
 };
 
 class TaskCompletion {
   public:
-    TaskCompletion(std::weak_ptr<Scheduler> worker, std::weak_ptr<Scheduler::Job>);
+    TaskCompletion(std::weak_ptr<Scheduler> worker, std::weak_ptr<Scheduler::Operation>);
 
     TaskCompletion(TaskCompletion&&) noexcept = default;
     TaskCompletion(const TaskContext&) = delete;
@@ -115,8 +54,8 @@ class TaskCompletion {
     ~TaskCompletion();
 
   private:
-    std::weak_ptr<Scheduler> m_worker;
-    std::weak_ptr<Scheduler::Job> m_job;
+    std::weak_ptr<Scheduler> m_scheduler;
+    std::weak_ptr<Scheduler::Operation> m_op;
 };
 
 }  // namespace kmm
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -1,5 +1,6 @@
 #include "kmm/scheduler.hpp"
 
+#include <atomic>
 #include <functional>
 #include <stdexcept>
 #include <utility>
@@ -8,50 +9,115 @@
 #include "kmm/utils.hpp"
 
 namespace kmm {
-struct Scheduler::Job {
-    Job(JobId, Command);
+enum class OperationStatus { Pending, Queueing, Ready, Staging, Running, Done };
 
-    JobId id;
+struct Scheduler::Operation {
+    Operation(OperationId id, Command kind, size_t unsatisfied) :
+        id(id),
+        command(std::move(kind)),
+        unsatisfied_predecessors(unsatisfied) {}
+
+    OperationId id;
+    OperationStatus status = OperationStatus::Pending;
     Command command;
-    JobStatus status = JobStatus::Pending;
-    size_t requests_pending = 0;
-    std::vector<MemoryRequest> requests = {};
-    size_t predecessors_pending = 1;
-    std::vector<std::weak_ptr<Job>> predecessors = {};
-    std::vector<std::shared_ptr<Job>> successors = {};
+    size_t unsatisfied_predecessors = 1;
+    std::vector<std::weak_ptr<Operation>> predecessors = {};
+    std::vector<std::shared_ptr<Operation>> successors = {};
+    std::vector<MemoryRequest> memory_requests = {};
+    std::atomic<bool> is_ready = false;
 };
 
-void Scheduler::make_progress() {
-    KMM_TODO();
-}
+struct WakerImpl: public Waker {
+    WakerImpl(std::shared_ptr<Scheduler> scheduler, std::shared_ptr<Scheduler::Operation> op) :
+        scheduler(std::move(scheduler)),
+        op(std::move(op)) {}
 
-void Scheduler::trigger_predecessor_completed(const std::shared_ptr<Job>& job) {
-    job->predecessors_pending -= 1;
+    void wakeup() const override {
+        scheduler->wakeup_op(op);
+    }
 
-    if (job->predecessors_pending == 0) {
-        job->status = JobStatus::Ready;
-        m_ready_tasks.push_back(job);
+    std::shared_ptr<Scheduler> scheduler;
+    std::shared_ptr<Scheduler::Operation> op;
+};
+
+void Scheduler::make_progress(std::chrono::time_point<std::chrono::system_clock> deadline) {
+    while (true) {
+        std::unique_lock guard {m_ready_lock};
+        bool not_empty =
+            m_ready_cond.wait_until(guard, deadline, [&] { return !m_ready_ops.empty(); });
+
+        if (!not_empty) {
+            break;
+        }
+
+        auto op = std::move(m_ready_ops.front());
+        m_ready_ops.pop_front();
+        guard.unlock();
+
+        poll_op(op);
     }
 }
 
-struct Scheduler::JobWaker: public Waker {
-    JobWaker(std::shared_ptr<Job> job, std::shared_ptr<Scheduler> scheduler) :
-        m_job(std::move(job)),
-        m_scheduler(std::move(scheduler)) {}
+void Scheduler::submit_command(CommandPacket packet) {
+    remove_duplicates(packet.dependencies);
 
-    void wakeup() const {
-        KMM_TODO();
+    auto new_op = std::make_shared<Operation>(
+        packet.id,
+        std::move(packet.command),
+        packet.dependencies.size() + 1);
+
+    auto predecessors = std::vector<std::weak_ptr<Operation>> {};
+    predecessors.reserve(packet.dependencies.size());
+    size_t satisfied = 1;
+
+    for (auto dep_id : packet.dependencies) {
+        auto it = m_ops.find(dep_id);
+        if (it == m_ops.end()) {
+            satisfied++;
+            continue;
+        }
+
+        auto& predecessor = it->second;
+        predecessor->successors.push_back(new_op);
+        predecessors.push_back(predecessor);
     }
 
-    std::shared_ptr<Job> m_job;
-    std::shared_ptr<Scheduler> m_scheduler;
-};
+    new_op->predecessors = std::move(predecessors);
 
-void Scheduler::stage_job(const std::shared_ptr<Job>& job) {
-    auto requests = std::vector<MemoryRequest> {};
+    // We always add one "phantom" predecessor to `predecessors_pending` so we can trigger it here
+    trigger_predecessor_completed(new_op, satisfied);
+}
 
-    if (const auto* cmd_exe = std::get_if<CommandExecute>(&(job->command))) {
-        auto waker = std::make_shared<JobWaker>(job, shared_from_this());
+void Scheduler::wakeup_op(const std::shared_ptr<Operation>& op) {
+    if (op->is_ready.exchange(true) == false) {
+        {
+            std::lock_guard guard {m_ready_lock};
+            m_ready_ops.push_back(op);
+        }
+
+        m_ready_cond.notify_all();
+    }
+}
+
+void Scheduler::trigger_predecessor_completed(const std::shared_ptr<Operation>& op, size_t count) {
+    if (op->unsatisfied_predecessors < count) {
+        op->unsatisfied_predecessors = 0;
+        op->status = OperationStatus::Queueing;
+
+        std::lock_guard guard {m_ready_lock};
+        m_ready_ops.push_back(op);
+    } else {
+        op->unsatisfied_predecessors -= count;
+    }
+}
+
+void Scheduler::stage_op(const std::shared_ptr<Operation>& op) {
+    op->status = OperationStatus::Staging;
+    bool is_ready = true;
+
+    if (const auto& cmd_exe = std::get_if<CommandExecute>(&op->command)) {
+        auto waker = std::make_shared<WakerImpl>(shared_from_this(), op);
+        auto requests = std::vector<MemoryRequest> {};
 
         for (const auto& arg : cmd_exe->buffers) {
             requests.push_back(m_memory_manager->create_request(  //
@@ -60,116 +126,91 @@ void Scheduler::stage_job(const std::shared_ptr<Job>& job) {
                 arg.is_write,
                 waker));
         }
+
+        is_ready = m_memory_manager->poll_requests(requests) == PollResult::Ready;
+        op->memory_requests = std::move(requests);
     }
 
-    size_t requests_pending = requests.size();
-    job->status = JobStatus::Staging;
-    job->requests_pending = requests_pending;
-    job->requests = std::move(requests);
-
-    if (requests_pending == 0) {
-        schedule_job(job);
+    if (is_ready) {
+        schedule_op(op);
     }
 }
 
-void Scheduler::schedule_job(const std::shared_ptr<Job>& job) {
-    job->status = JobStatus::Scheduled;
+void Scheduler::poll_op(const std::shared_ptr<Operation>& op) {
+    op->is_ready.store(false);
+
+    if (op->status == OperationStatus::Staging) {
+        if (m_memory_manager->poll_requests(op->memory_requests) == PollResult::Ready) {
+            schedule_op(op);
+        }
+    } else if (op->status == OperationStatus::Queueing) {
+        stage_op(op);
+    }
+}
+
+void Scheduler::schedule_op(const std::shared_ptr<Operation>& op) {
+    op->status = OperationStatus::Running;
     bool is_done = true;
 
-    if (const auto* cmd_exe = std::get_if<CommandExecute>(&(job->command))) {
-        is_done = false;
+    if (const auto* cmd_exe = std::get_if<CommandExecute>(&op->command)) {
+        auto accessors = std::vector<BufferAccess> {};
 
-        std::vector<BufferAccess> allocations;
-        for (const auto& req : job->requests) {
-            allocations.push_back(BufferAccess {
-                .allocation = m_memory_manager->view_buffer(req),
+        for (auto& req : op->memory_requests) {
+            const auto* allocation = m_memory_manager->view_buffer(req);
+            accessors.push_back(BufferAccess {
+                .allocation = allocation,
                 .writable = false,
             });
         }
 
-        auto context = TaskCompletion(shared_from_this(), job);
+        is_done = false;
         m_executors.at(cmd_exe->device_id)
-            ->submit(
-                cmd_exe->task,  //
-                std::move(allocations),
-                std::move(context));
+            ->submit(cmd_exe->task, std::move(accessors), TaskCompletion(shared_from_this(), op));
 
-    } else if (const auto* cmd_noop = std::get_if<CommandNoop>(&job->command)) {
+    } else if (const auto* cmd_noop = std::get_if<CommandNoop>(&op->command)) {
         // Nothing to do
 
-    } else if (const auto* cmd_create = std::get_if<CommandBufferCreate>(&job->command)) {
+    } else if (const auto* cmd_fut = std::get_if<CommandPromise>(&op->command)) {
+        cmd_fut->promise.set_value();
+
+    } else if (const auto* cmd_create = std::get_if<CommandBufferCreate>(&op->command)) {
         m_memory_manager->create_buffer(cmd_create->id, cmd_create->description);
 
-    } else if (const auto* cmd_delete = std::get_if<CommandBufferDelete>(&job->command)) {
+    } else if (const auto* cmd_delete = std::get_if<CommandBufferDelete>(&op->command)) {
         m_memory_manager->delete_buffer(cmd_delete->id);
 
-    } else if (const auto* cmd_obj = std::get_if<CommandObjectDelete>(&job->command)) {
+    } else if (const auto* cmd_obj = std::get_if<CommandObjectDelete>(&op->command)) {
         m_object_manager->delete_object(cmd_obj->id);
 
     } else {
-        throw std::runtime_error("invalid job command");
+        throw std::runtime_error("invalid command");
     }
 
     if (is_done) {
-        complete_job(job);
+        complete_op(op);
     }
 }
 
-void Scheduler::complete_job(const std::shared_ptr<Job>& job) {
-    job->status = JobStatus::Done;
-    m_jobs.erase(job->id);
+void Scheduler::complete_op(const std::shared_ptr<Operation>& op) {
+    // TODO: Get lock
+    op->status = OperationStatus::Done;
+    m_ops.erase(op->id);
 
-    for (const auto& request : job->requests) {
-        m_memory_manager->delete_request(request, std::nullopt);
-    }
-
-    for (const auto& successor : job->successors) {
+    for (const auto& successor : op->successors) {
         trigger_predecessor_completed(successor);
     }
 }
 
-void Scheduler::submit_command(CommandPacket packet) {
-    auto new_job = std::make_shared<Job>(packet.id, std::move(packet.command));
-
-    remove_duplicates(packet.dependencies);
-
-    auto predecessors = std::vector<std::weak_ptr<Job>> {};
-    predecessors.reserve(packet.dependencies.size());
-
-    for (auto dep_id : packet.dependencies) {
-        auto it = m_jobs.find(dep_id);
-        if (it == m_jobs.end()) {
-            continue;
-        }
-
-        auto& predecessor = it->second;
-        predecessor->successors.push_back(new_job);
-        predecessors.push_back(predecessor);
-    }
-
-    size_t predecessors_pending = predecessors.size();
-    new_job->predecessors = std::move(predecessors);
-    new_job->predecessors_pending = predecessors_pending + 1;
-
-    // We always add one "phantom" predecessor to `predecessors_pending` so we can trigger it here
-    trigger_predecessor_completed(new_job);
-}
-
-Scheduler::Job::Job(JobId id, Command kind) :
-    id(id),
-    command(std::move(kind)),
-    status(Scheduler::JobStatus::Pending) {}
-
 TaskCompletion::TaskCompletion(
     std::weak_ptr<Scheduler> worker,
-    std::weak_ptr<Scheduler::Job> task) :
-    m_worker(std::move(worker)),
-    m_job(std::move(task)) {}
+    std::weak_ptr<Scheduler::Operation> task) :
+    m_scheduler(std::move(worker)),
+    m_op(std::move(task)) {}
 
 void TaskCompletion::complete() {
-    if (auto job = m_job.lock()) {
-        m_job.reset();
-        m_worker.lock()->complete_job(job);
+    if (auto op = m_op.lock()) {
+        m_op.reset();
+        m_scheduler.lock()->complete_op(op);
     }
 }
 
