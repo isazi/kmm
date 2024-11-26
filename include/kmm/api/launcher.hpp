f0f352121575db63b160cf95f18e881f47fe7b2d
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2024-11-26 16:20:20 +0100

    Fixing the cast of the grid dims.

--- include/kmm/api/launcher.hpp
+++ include/kmm/api/launcher.hpp
@@ -55,9 +55,9 @@ struct CudaKernel {
         int64_t g[3] = {chunk.size.get(0), chunk.size.get(1), chunk.size.get(2)};
         int64_t b[3] = {elements_per_block.x, elements_per_block.y, elements_per_block.z};
         dim3 grid_dim = {
-            checked_cast<int>((g[0] / b[0]) + int64_t(g[0] % b[0] != 0)),
-            checked_cast<int>((g[1] / b[1]) + int64_t(g[1] % b[1] != 0)),
-            checked_cast<int>((g[2] / b[2]) + int64_t(g[2] % b[2] != 0)),
+            checked_cast<unsigned int>((g[0] / b[0]) + int64_t(g[0] % b[0] != 0)),
+            checked_cast<unsigned int>((g[1] / b[1]) + int64_t(g[1] % b[1] != 0)),
+            checked_cast<unsigned int>((g[2] / b[2]) + int64_t(g[2] % b[2] != 0)),
         };
 
         auto region = NDRange(chunk.offset, chunk.size);
